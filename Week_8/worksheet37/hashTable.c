53a33ae639397b6ae513a34d694f82df784db101
shurlee <shirleychan510@gmail.com>
2020-02-27 00:22:20 -0800

    Update hashTable.c

--- Week_8/worksheet37/hashTable.c
+++ Week_8/worksheet37/hashTable.c
@@ -63,12 +63,10 @@ void openHashTableAdd (struct openHashTable * ht, TYPE newValue) {
     //dereference pointer at the index so we can assign to the newvalue pointer
     //because table[idx] is a double pointer
     
-    if(ht->table[idx]!=NULL) {
-        *(ht->table[idx]) = newValue;
-    } else {
-        ht->table[idx] = malloc(sizeof(TYPE *));
-        *(ht->table[idx]) = newValue;
+    if(ht->table[idx]==0) {
+        ht->table[idx] = malloc(sizeof(TYPE));
     }
+    *(ht->table[idx]) = newValue;
 }
 
 
@@ -88,26 +86,89 @@ int openHashTableBagContains (struct openHashTable *ht, TYPE newValue) {
 }
 
 void _resizeOpenHashTable (struct openHashTable *ht) {
+    
     //write this
     struct openHashTable newHT;
     initOpenHashTable(&newHT, ht->tablesize * 2);
+    
     for(int i = 0; i < ht->tablesize; i++) {
-        openHashTableAdd(&newHT, *(ht->table[i]));
+        if(ht->table[i]!=0){ //only rehash if the space was allocated
+            openHashTableAdd(&newHT, *(ht->table[i]));
+            free(ht->table[i]); //need to free old pointer info is transferred
+        }
     }
     //free ht somehow
     free(ht->table);
     
-    ht = &newHT;
+    ht->table = newHT.table;
+    ht->tablesize = newHT.tablesize;
     // newHT = 0;
 }
 
 void print(struct openHashTable *ht) {
+    
+    printf("==PRINTING HASH TABLE==\n");
     for(int i = 0; i < ht->tablesize; i++) {
-        printf("%s\n", *(ht->table[i]));
+        
+        if(ht->table[i]!=0) {
+            printf("key: %i value: %s\n", i, *(ht->table[i]));
+        
+        }
     }
+    printf("\n\n");
 }
+
+
+void freeHashTable(struct openHashTable * ht){
+    
+    //freeing individual pointers
+    for(int i = 0; i<ht->tablesize; i++) {
+     
+        if(ht->table[i]!=0){ //check to see if it was allocated before freeing
+            free(ht->table[i]);
+        }
+    }
+    
+    //freeing table
+    free(ht->table);
+}
+
 
 int main() {
 
+    
+    char string1[] = "Tanya";
+    char string2[] = "Andrew";
+    char string3[] = "Noor";
+    char string4[] = "Melisa";
+    char string5[] = "Shirley";
+    char string6[] = "Pikachu";
+    char string7[] = "Bulbasaur";
+    char string8[] = "Blastoise";
+    char string9[] = "Geodude";
+    char string10[] = "Starmie";
+    char string11[] = "Squirtle";
+    
+    struct openHashTable table;
+    initOpenHashTable(&table,6);
+    openHashTableAdd(&table, string1);
+    openHashTableAdd(&table, string2);
+    openHashTableAdd(&table, string3);
+    openHashTableAdd(&table, string4);
+    openHashTableAdd(&table, string5);
+    
+    print(&table);
+    
+    openHashTableAdd(&table, string6);
+    openHashTableAdd(&table, string7);
+    openHashTableAdd(&table, string8);
+    openHashTableAdd(&table, string9);
+    openHashTableAdd(&table, string10);
+    openHashTableAdd(&table, string11);
+    
+    print(&table);
+    freeHashTable(&table);
+    
     return 0;
 }
+
